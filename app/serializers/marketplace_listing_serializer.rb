6481b267b396cb5b3bac54366ed442e8fa1d1f72
Asrat <asratextras77@gmail.com>
2025-04-01 10:43:10 +0300

    Use template file format on resource generators

--- app/serializers/marketplace_listing_serializer.rb
+++ app/serializers/marketplace_listing_serializer.rb
@@ -2,5 +2,4 @@ class MarketplaceListingSerializer < ActiveModel::Serializer
   attributes :id, :listing_type, :status, :allow_partial_match, :expires_at
   belongs_to :user
   belongs_to :address
-
 end
