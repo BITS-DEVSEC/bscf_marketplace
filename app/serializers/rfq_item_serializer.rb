19bfdeddad014531bc9eb5e50d5be968293cc479
Asrat <asratextras77@gmail.com>
2025-05-08 14:18:17 +0300

    Add product_id

--- app/serializers/rfq_item_serializer.rb
+++ app/serializers/rfq_item_serializer.rb
@@ -1,5 +1,5 @@
 class RfqItemSerializer < ActiveModel::Serializer
-  attributes :id, :quantity, :notes, :created_at, :updated_at
+  attributes :id, :quantity, :notes, :product_id, :created_at, :updated_at
 
   belongs_to :request_for_quotation
   belongs_to :product
