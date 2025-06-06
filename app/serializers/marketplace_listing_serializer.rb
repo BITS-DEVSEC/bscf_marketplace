be80edf4b8d00ec1edfad11cede1958d95cb6c90
Asrat <asratextras77@gmail.com>
2025-06-06 14:38:49 +0300

    Add product and price to MarketPlaceListings serializer

--- app/serializers/marketplace_listing_serializer.rb
+++ app/serializers/marketplace_listing_serializer.rb
@@ -1,5 +1,6 @@
 class MarketplaceListingSerializer < ActiveModel::Serializer
-  attributes :id, :listing_type, :status, :allow_partial_match, :expires_at
+  attributes :id, :listing_type, :status, :allow_partial_match, :expires_at, :price
   belongs_to :user
   belongs_to :address
+  belongs_to :product
 end
